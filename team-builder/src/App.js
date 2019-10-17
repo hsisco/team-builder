aed698767c440997f558ee6c3cd46c5218af69d6
hsisco <hysensisco@gmail.com>
2019-10-16 22:58:30 -0700

    handleChange + handleSubmit event functions

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -2,10 +2,15 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [tm, setTM] = useState({name: "", email: "", password: ""});
+  const [tm, setTM] = useState({name: "", email: "", role: ""});
 
   const handleChange = event => {
     setTM({...tm, [event.target.name]: event.target.value});
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setTM({name: "", email: "", role: ""})
   };
 
   return (
